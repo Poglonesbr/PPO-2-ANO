10e053bff10e88dca25ecf70fb7ef2e555233872
Poglonesbr <131016905+Poglonesbr@users.noreply.github.com>
2024-08-02 00:43:12 +0000

    Adicionei Site Atualizado

---
+++ site if/Cursos/Cursos.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var acc = document.getElementsByClassName("accordion-button");
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+});
